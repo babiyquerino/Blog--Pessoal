54c542e6e96b7a295df245789dfebb585f5ca6c7
unknown <--contatobabiy@gmai.com>
2021-06-09 17:32:44 -0300

    Implementação do get e post de temas

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TemaService {
+
+  constructor(
+    private http: HttpClient) { }
+
+    token={
+      Headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+
+    getAllTema():Observable<Tema[]> {
+      return this.http.get<Tema[]>('http://127.0.0:3306/tema')
+    }
+
+    postTema(tema: Tema): Observable<Tema>{
+      return this.http.post<Tema>('http://127.0.0:3306/tema', this.token)
+
+    }
+
+}
